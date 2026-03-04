a3fe15c5f79b592bbeeddac5a57a17a428870b72
senci33-wq <senci33@googlemail.com>
2026-03-04 19:04:42 +0100

    Update with Branding

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-from kivy.app import App
-from kivy.uix.button import Button
-import webbrowser
-
-class MainApp(App):
-    def build(self):
-        btn = Button(text='Augsburger Tafel (Sicherer Link)')
-        # Wir erzwingen http, um den SSL-Fehler zu umgehen
-        btn.bind(on_release=lambda x: webbrowser.open('http://www.augsburger-tafel.de'))
-        return btn
-
-MainApp().run()
+Paste dein Multi-Tool-Rezept hier...
